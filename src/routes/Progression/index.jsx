85672afdb31ac51e2d5bb06c44590690ae82d884
shuurei <lxnnylqs44@gmail.com>
2026-05-28 07:03:05 +0200

    Remove jsx files, and fix build

--- src/routes/Progression/index.jsx
+++
@@ -1,6 +0,0 @@
-import './Progression.styles.css'
-export default function Progression() {
-  return (
-    <div className='progression'>Tout information concernant la progression du jeu sera mis ici.</div>
-  )
-}
